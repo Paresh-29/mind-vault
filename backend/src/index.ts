a32da9dda07001f1cc24ab231960ecc02636fed7
Paresh-29 <pareshkumarbarick99@gmail.com>
2025-04-14 22:38:47 +0530

    feat: added url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,27 @@
-import express from "express";
-import { connectDB } from "./lib/db";
+import express from 'express';
+import { connectDB } from './lib/db';
 import authRoutes from './routes/auth.route';
 import contentRoute from './routes/content.route';
 import shareRoute from './routes/share.route';
-import cors from "cors";
-
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-	origin: "http://localhost:5173", // Change to your frontend URL
-	methods: ["GET", "POST", "PUT", "DELETE"],
-	allowedHeaders: ["Authorization", "Content-Type"],
-	credentials: true
-}));
+app.use(
+  cors({
+    origin: ['https://mind-vault-virid.vercel.app', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  }),
+);
 
-
-
-app.use("/api/v1/", authRoutes);
-app.use("/api/v1/content", contentRoute);
-app.use("/api/v1/brain", shareRoute);
-
+app.use('/api/v1/', authRoutes);
+app.use('/api/v1/content', contentRoute);
+app.use('/api/v1/brain', shareRoute);
 
 app.listen(3000, () => {
-	console.log("server listening on port 3000");
-	connectDB();
+  console.log('server listening on port 3000');
+  connectDB();
 });
